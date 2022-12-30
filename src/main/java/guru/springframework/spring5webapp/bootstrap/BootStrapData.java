4f5152e9da732f336dd3a567031cecd0875e8be8
Filip Szydłak <266912@student.pwr.edu.pl>
2022-12-30 19:57:46 +0100

    Publisher Relationships added

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -25,31 +25,46 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
+
+        publisherRepository.save(publisher);
+
+        System.out.println("Number of Publishers: " + publisherRepository.count());
+
         Author eric = new Author("Eric","Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
 
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "12231323");
 
-        authorRepository.save(rod);
-        bookRepository.save(noEJB);
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        publisher.getBooks().add(noEJB);
+        noEJB.setPublisher(publisher);
 
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
+
+
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
 
-        Publisher rGuy = new Publisher("dd","dd","dd","dd","dd");
-        publisherRepository.save(rGuy);
 
-        System.out.println("Number of Publishers: " + publisherRepository.count());
 
 
     }
